65fe8bcb53328b7e9c02c724c8c7712abe1e2bc1
Guillermo Ehrenbolger <68972780+valkanoise@users.noreply.github.com>
2022-07-01 14:18:49 +0200

    Update broot.sh

--- broot.sh
+++ broot.sh
@@ -4,7 +4,7 @@
 
 curl -o broot -L https://dystroy.org/broot/download/armv7-unknown-linux-gnueabihf/broot
 
-sudo mv broot /usr/local/bin
-sudo chmod +x /usr/local/bin/broot
+mv broot /usr/local/bin
+chmod +x /usr/local/bin/broot
 
 echo "Installed version: $(broot --version)"
